970196cadb79471264bbf81be7e0316e62721e01
Albert <albert02lowis@gmail.com>
2020-08-05 23:20:56 +0800

    Fix compile error.

--- purchase-service-api/src/main/kotlin/com/albertlowis/purchaseevent/Purchase.kt
+++ purchase-service-api/src/main/kotlin/com/albertlowis/purchaseevent/Purchase.kt
@@ -1,6 +1,7 @@
 package com.albertlowis.purchaseevent
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import kotlinx.serialization.Serializable
 import org.apache.kafka.common.serialization.Deserializer
 import org.apache.kafka.common.serialization.Serde
