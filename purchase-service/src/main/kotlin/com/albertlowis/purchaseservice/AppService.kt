fcd55e5e8b2dcec120d1f13f29dae80e3d742d7c
Albert <albert02lowis@gmail.com>
2020-08-06 00:17:14 +0800

    Add running of purchase streams.

--- purchase-service/src/main/kotlin/com/albertlowis/purchaseservice/AppService.kt
+++ purchase-service/src/main/kotlin/com/albertlowis/purchaseservice/AppService.kt
@@ -1,7 +1,11 @@
 package com.albertlowis.purchaseservice
 
+import com.albertlowis.purchasestreams.PurchaseStreams
+
 fun main(args: Array<String>) {
     println("hello purchase service")
     PurchaseProducer().produce()
     //TODO: Wire purchase service REST endpoint
+
+    PurchaseStreams().runService()
 }
